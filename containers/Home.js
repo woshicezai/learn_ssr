a60e1ea7efb2bd59d8d8acd18ce8d53b63f59479
zhouce <zhouce@hualala.com>
2019-08-07 20:39:29 +0800

    init ssr

---
+++ containers/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <div>
+            <span>This is sanyuan</span>
+        </div>
+    )
+}
+
+
+
+/****
+ 现在的任务就是将它转换为html代码返回给浏览器。
+ 总所周知，JSX中的标签其实是基于虚拟DOM的，
+ 最终要通过一定的方法将其转换为真实DOM。
+ *****/
--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
-let express = require("express");
+import express from "express";
+import {renderToString,renderToStaticMarkup} from "react-dom/server";
+import {Home} from "../containers/Home";
+import React from "react";
+
+//content =  <div data-reactroot=""><span>This is sanyuan</span></div>
+let content = renderToString(<Home/>);
+
 let app = express();
 app.get("/", (req, res) => {
     res.send(`
       <html>
      <head>
-       <title>hello</title>
+       <title>HOME</title>
      </head>
      <body>
-       <h1>hello</h1>
-       <p>world</p>
+        <div id="root">${content}</div>
      </body>
    </html>
 
